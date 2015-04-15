5c4602d9770d606aa3a615f86537b97bdf033fb0
Paul Rankin <hello@paulwrankin.com>
2015-04-15 13:39:27 +1000

    Rewrote to use canvas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,45 @@
+---
+#
+---
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementsByTagName("video")[0];
+
+    var canvas = document.getElementsByTagName("canvas")[0];
+    var context = canvas.getContext("2d");
+    var canvasW = {{ site.canvas.width }};
+    var canvasH = {{ site.canvas.height }};
+    canvas.width = canvasW;
+    canvas.height = canvasH;
+
+    var info = document.getElementById("info");
+
+    video.addEventListener("canplay", function() {
+        this.play();
+    }, false);
+
+    canvas.addEventListener("dblclick", function() {
+        screenfull.toggle();
+    }, false);
+
+    info.addEventListener("click", function() {
+        toggleDisplay("info");
+    }, false);
+
+    drawCanvas(video, context, canvasW, canvasH);
+}, false);
+
+function drawCanvas(source, context, width, height) {
+    context.drawImage(source, 0, 0, width, height);
+    setTimeout(drawCanvas, 40, source, context, width, height);
+}
+
 function toggleDisplay(id) {
-    var e = document.getElementById(id);
-    if(e.style.display == "block") {
-        e.style.display = "none";
+    var element = document.getElementById(id);
+    if(element.style.display == "block") {
+        element.style.display = "none";
     }
     else {
-        e.style.display = "block";
+        element.style.display = "block";
     }
 }
 
